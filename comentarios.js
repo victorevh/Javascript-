6d6e373f07194f07ef31e954c4792aaec8430ef2
victorevh <79948760+victorevh@users.noreply.github.com>
2021-04-20 20:07:53 -0300

    Add files via upload

---
+++ comentarios.js
@@ -0,0 +1,9 @@
+// Aqui tenho um comentário em linha, // serve para ignorar codigos
+/* 
+  Aqui tenho um comentario multi linha ou comentario de bloco
+*/
+
+// o código abaixo escreve alguma mensagem no devtools
+
+console.log('Bem vindos ao Starter!!') // console.log('ieia ignorar')
+console.log('não iria ignorar')
