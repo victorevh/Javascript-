f12ff5ba577d0308face63558d2e98006242bba1
victorevh <79948760+victorevh@users.noreply.github.com>
2021-06-08 12:13:39 -0300

    Add files via upload

---
+++ Functions/functions.js
@@ -0,0 +1,9 @@
+// fumction expression
+// function anonymous
+
+// parâmetros (parameters) (number1, number2)
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(5, 3) // arguments - argumentos
---
+++ Functions/functions_1.js
@@ -0,0 +1,21 @@
+// criar um aplicativo de frases motivacionais
+
+// as funções servem pra agrupar codigos e dar um significado a eles
+// servem também para repetir os codigos sempre que eu queira
+
+// passo 1 declaration - declaração da função
+// function statement
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+// executar a função
+// rodar, chamar, invocar a função
+// execute, run, call, invoke
+
+createPhrases()
+createPhrases()
+
+console.log('fim do programa')
