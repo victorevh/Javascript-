a699645740d5b2179ea43ebd46961baa44eef7d0
victorevh <79948760+victorevh@users.noreply.github.com>
2021-07-13 11:50:59 -0300

    Add files via upload

--- Prototype/scripts.js
+++ Prototype/scripts.js
@@ -112,5 +112,100 @@ let two = 2
 // console.log (one === 1)
 
 // !== estritamente diferente de
-console.log( two !== "2")
-console.log( two !== 2)
+// console.log( two !== "2")
+// console.log( two !== 2)
+
+// >     Maior que
+// console.log(one > two)
+
+// >=    Maior igual a
+// console.log(one >= 1)
+// console.log(two >= 1)
+
+// <     Menor igual a
+// console.log( one <= two)
+// console.log( one <= 1)
+// console.log( one <= 0)
+
+// Operadores de atribuição (Assignment)
+let x
+
+
+// assignment
+x = 1
+
+// addition assignment
+// x = x + 2 expressão
+x += 2
+
+// subtraction assignment
+// x = x - 1
+x -= 1
+
+// multiplication assignment
+// x = x * 2
+x *= 2
+
+// division assignment
+// x = x /2
+x /= 2
+
+// remainder, exponentiation
+// x %= 2
+x **=2
+// console.log(x)
+
+// Operadores lóficos (logical operators)
+
+// - 2 valores booleanos, quando verificados,
+// resultará em verdadeiro ou falso
+
+// let pao = true
+// let queijo = true
+
+// AND &&
+// console.log(pao && queijo)
+
+
+// OR ||
+// console.log(pao || queijo)
+
+// NOT !  quando se nega uma variavel ela tem seu valor trocado
+// console.log(!pao)
+
+// Operador Condicional (Ternário)
+
+// Dependendo da condição, nós receberemos valores diferentes
+
+// Condição então valor 1 se não valor 2
+// condition ? value1 : value2
+
+// Exemplos
+// Café da manhã top
+// let pao = false
+// let queijo = false
+
+// const niceBreakfast = pao && queijo ? 'Café top' : 'Café ruim'
+// const niceBreakfast = pao || queijo ? 'Café bom' : 'Café ruim'
+// console.log(niceBreakfast)
+
+// Maior que 18
+
+// let age = 16
+
+// const canDrive = age >= 18 ? 'Can Drive' : "Can't drive"
+// console.log(canDrive)
+
+
+// Operador de String (String operator)
+
+// comparison (comparação)
+// console.log( 'a' == 'a')
+
+
+// concatenation (concatenação)
+// Retorna a união de duas Strings
+// let alpha = 'alfa'
+// console.log( alpha + 'bet' + 's')
+// alpha += 'bet'
+// console.log(alpha + 's')
