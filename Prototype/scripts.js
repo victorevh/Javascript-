fff7203e9a4cd26144a2ac6682e541203a83023c
victorevh <79948760+victorevh@users.noreply.github.com>
2021-07-16 17:08:14 -0300

    Add files via upload

--- Prototype/scripts.js
+++ Prototype/scripts.js
@@ -1,102 +1,42 @@
-// Control Flow
+// Estrutura de repetição
+// for
 
-// se estiver sujo
-// console.log('lavar o copo')
+// break = parar a execução do looping
+// continue = pula a execução do looping 
 
-// console.log('servir cafe')
-
-// if...else     se...se não
-
-let temperature = 45.5
-let highTemperature = temperature >= 37.5
-let mediumTemperature = temperature < 37.5 && temperature >= 37
 
 /*
-if(temperature >= 37.5) {
-    console.log('Febre Alta')
-} else if(temperature < 37.5 && temperature >= 37) {
-    console.log('Febre Moderada')
-} else {
-    console.log('Saudável')
+for(let a = 0; a <= 10; a++) {
+    console.log(a)
 }
 
-if(highTemperature){
-    console.log('Febre Alta')
-} else if(mediumTemperature){
-    console.log('Febre Moderada')
-} else {
-    console.log('Saudável')
-}
-*/
-
-// switch
-// case = caso ; break = mostra que o caso acabou, caso não utilize o console vai imprimir todos os casos a seguir
-
-/*
-let expression = 'b'
-
-switch (expression) {
-    case 'a':
-        // codigo
-        console.log('a')
-        break
-    case 'b':
-        // codigo para expression b
-        console.log('b')
-        break
-    default:
-        console.log('default')
-        break
-}
-*/
-
-/*
-function calculate(number1, operator, number2) {
-    let result
-
-    switch (operator) {
-        case '+':
-            result = number1 + number2
-            break
-        case '-':
-            result = number1 - number2
-            break
-        case '*':
-            result = number1 * number2
-            break
-        case '/':
-            result = number1 / number2
-            break
-        default:
-            console.log('não implementado')
-            break
+for(let i = 20; i > 0; i--) {
+    if(i === 10) {
+        break;
+    }
+    if(i === 15) {
+        continue;
     }
 
-    return result
+    console.log(i)
 }
-
-console.log(calculate(4, '*', 9))
-console.log(calculate(9, '/', 2))
-console.log(calculate(60, '%', 20))
 */
 
-// throw  lançar ou disparar
-
+// while
 /*
-function sayMyName(name = '') {
-    if (name === '') {
-        throw new Error("O nome é obrigatorio")
-    }
+let i = 0;
+while(i < 10) {
+    console.log(i)
 
-    console.log(name)
+    i++;
 }
-
-// Try...catch    tentar e capturar
-try {
-    sayMyName('Victor')
-} catch(e) {
-    console.log(e)
-}
-
-console.log('após ao try/catch')
 */
+
+// While faz muito sentido quando não sabemos qual o momento da parada exemplo
+
+let i = 5000000;
+while(i > 12) {
+    console.log(i)
+
+    i /= 25
+}
