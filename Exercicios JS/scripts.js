c4eb206d92e77d655e9890af60d0f35edfbd9587
victorevh <79948760+victorevh@users.noreply.github.com>
2021-07-22 11:07:25 -0300

    Add files via upload

--- Exercicios JS/scripts.js
+++ Exercicios JS/scripts.js
@@ -1,48 +1,46 @@
-/* ### Transformar notas escolares
-
-Crie um algoritmo que transforme as notas do sistema numérico para sistemas den otas em caracteres tipo A B C
-
-* de 90 para cima - A
-* entre 80 - 89 - B
-* entre 70 - 79 - C
-* entre 60 - 69 - D
-* menor que 60 - F
-
-*/ 
+/* ### Sistema de gastos familiar
 
 
-function getNotas(notas) {
-    let a = notas >= 90 && notas <= 100
-    let b = notas < 90 && notas >= 80
-    let c = notas < 80 && notas >= 70
-    let d = notas < 70 && notas >= 60
-    let f = notas < 60 && notas >= 0
-    
-    let notaFinal
-    
-    if(a){
-        notaFinal = "A"
-    } else if(b){
-        notaFinal = "B"
-    } else if(c){
-        notaFinal = "C"
-    } else if(d){
-        notaFinal = "D"
-    } else if(f){
-        notaFinal = "F"
-    } else {
-        notaFinal = "Nota Inválida"
-    }
-    
-    return notaFinal
-    
+Crie um objeto que possuirá 2 propriedades, ambas do tipo array:
+   * Receitas: []
+   * Despesas: []
+
+Agora, crie uma função que irá calcular o total e receitas e despesas e irá mostrar uma mensagem se a família está com o saldo positivo ou negativo, seguido do valor do saldo
+
+*/
+
+let family = {
+    incomes: [500.40, 500.34, 1300.10, 900.00],
+    expenses: [430.20, 1130.45, 1000.40, 1500.00]
 }
 
-console.log(getNotas(110))
-console.log(getNotas(100))
-console.log(getNotas(90))
-console.log(getNotas(80))
-console.log(getNotas(70))
-console.log(getNotas(60))
-console.log(getNotas(50))
-console.log(getNotas(-15))
+// Função para somar os numeros dentro de cada array
+
+function sum(array) {
+    let total = 0
+    
+    for(let value of array) {
+        total += value
+    }
+    
+    return total
+}
+
+function calculateBalance() {
+    const calculateIncomes = sum(family.incomes)
+    const calculateExpenses = sum(family.expenses)
+
+    const total = calculateIncomes - calculateExpenses
+
+    const itsOk = total >= 0
+
+    let balanceText = "Negativo"
+    
+    if(itsOk) {
+        balanceText = "Positivo"
+    }
+
+    console.log(`Seu saldo é ${balanceText}: ${total.toFixed(2)} R$`)
+}
+
+calculateBalance()
